b440793768158f4f5805b044cf0614e48d1fca43
Rahul Yedida <yrahul3910@gmail.com>
2018-08-17 22:59:12 +0530

    Started implementing Open Journal

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,23 +5,109 @@ import Modal from 'react-native-modal';
 import { TextInput } from 'react-native';
 import owasp from 'owasp-password-strength-test';
 import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
+import async from 'async';
+import os from 'react-native-os';
+import fs from 'react-native-fs';
+import crypto from 'crypto';
+import tar from 'targz';
 
 class HomeScreen extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { modalVisible: false, error: '' }
+
+        /*
+            modalVisible: determines if the New Journal modal is visible
+            error: the error for the New Journal modal
+            openModalVisible: determines if the modal for taking password input
+                (while opening a journal) is visible.
+        */
+        this.state = { modalVisible: false, error: '', openModalVisible: false }
+
         this.openJournal = this.openJournal.bind(this);
         this.createJournal = this.createJournal.bind(this);
         this.confirmNewJournal = this.confirmNewJournal.bind(this);
+        this.decryptJournal = this.decryptJournal.bind(this);
+        this.decryptFile = this.decryptFile.bind(this);
 
         this.journalData = {
             pwd: '',
             entries: { en: [] },
-            currentEntryCount: 0
+            currentEntryCount: 0,
+            currentFileVersion: 5.1
         };
+
+        this.currentFilePath = "";
+        this.encryptedData = "";
 
         this.pwdRef = React.createRef();
         this.rePwdRef = React.createRef();
+        this.openPwdRef = React.createRef();
+    }
+
+    decryptFile(path, outputPath, key, func) {
+        const algorithm = "aes256";
+        let decipher = crypto.createDecipher(algorithm, key);
+        let input = fs.createReadStream(path);
+        let output = fs.createWriteStream(outputPath);
+        try {
+            input.pipe(decipher).on("error", (e) => {
+                func(e);
+            }).on("end", () => {
+                func();
+            }).pipe(output);
+        } catch(ex) {
+            func(ex);
+        }
+    };
+
+    decompress = (filename, func) => {
+        tar.decompress({
+            src: filename,
+            dest: os.tmpdir() + "/_jbfiles",
+            tar: {
+                fmode: parseInt(777, 8),
+                dmode: parseInt(777, 8)
+            }
+        }, func);
+    };
+    
+
+    decryptJournal() {
+        let pwd = this.pwdRef.current._lastNativeText;
+
+        if (this.journalData.currentFileVersion != 5.1) {
+            alert("This file format is not supported on the mobile version. Please use " +
+                "the desktop version to upgrade to the new file format."
+            );
+            return;
+        }
+
+        let tmp = os.tmpdir();
+        async.waterfall([
+            (callback) => {
+                // Decrypt the file
+                this.decryptFile(this.currentFilePath, tmp + "/_jb.tar.gz", pwd, callback);
+            },
+            (callback) => {
+                // Extract the archive
+                this.decompress(tmp + "/_jb.tar.gz", callback);
+            },
+            (callback) => {
+                // Read the file
+                fs.readFile(tmp + "/_jbfiles/data.json", (err, data) => {
+                    if (err) throw err;
+                    callback(null, data);
+                });
+            },
+            (data, callback) => {
+                this.journalData.entries = data;
+                this.setState({ openModalVisible: false });
+                this.props.navigation.navigate('Entries', {journalData: this.journalData});
+                callback(null);
+            }
+        ], (err) => {
+            this.setState({error: 'Wrong password. Try again.'});
+        });
     }
 
     openJournal() {
@@ -29,16 +115,19 @@ class HomeScreen extends React.Component {
         //this.props.navigation.navigate('Entries', {journalData: this.journalData});
 
         DocumentPicker.show({
-            filetype: [DocumentPickerUtil.images()],
-          },(error,res) => {
-            // Android
-            console.log(
-               res.uri,
-               res.type, // mime type
-               res.fileName,
-               res.fileSize
-            );
-          });
+            filetype: [DocumentPickerUtil.allFiles()],
+        },(error,res) => {
+            let filename = res.uri;
+
+            if (filename.endsWith(".ejournal"))
+                this.journalData.currentFileVersion = 5.0;
+            else {
+                this.currentFilePath = filename;
+                this.journalData.currentFileVersion = 5.1;
+            }
+
+            encryptedData = fs.readFileSync(filenames[0]).toString();
+        });
     }
 
     createJournal() {
@@ -74,6 +163,7 @@ class HomeScreen extends React.Component {
         }
 
         this.journalData.pwd = pwd;
+        this.journalData.currentFileVersion = 5.1;
 
         // Journal created. Move to entries screen.
         this.setState({modalVisible: false});
@@ -82,7 +172,40 @@ class HomeScreen extends React.Component {
 
     render() {
         return (
-            <View style={styles.parent}>   
+            <View style={styles.parent}>
+                <Modal isVisible={this.state.openModalVisible}
+                    onBackButtonPress={() => {
+                        this.setState({ openModalVisible: false });
+                    }}
+                    onBackdropPress={() => {
+                        this.setState({ openModalVisible: false });
+                    }}>
+                    <View style={styles.modal}>
+                        <Text style={{marginBottom: 10, color: 'red'}}>
+                            {this.state.error}
+                        </Text>
+                        <Text style={{ marginBottom: 20 }}>
+                            Please enter the decryption password.
+                        </Text>
+                        <TextInput ref={this.openPwdRef}
+                            secureTextEntry={true} 
+                            placeholder='Enter password'
+                            style={styles.textInput} />
+                        <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+                            <Button title='OK' 
+                                onPress={this.decryptJournal}
+                                style={styles.okButton}
+                                textStyle={{fontSize: 14, fontWeight: '500', color: 'rgb(42, 101, 132)'}}>
+                                OK
+                            </Button>
+                            <Button onPress={() => {this.setState({ modalVisible: false });}}
+                                style={styles.cancelButton}
+                                textStyle={{fontSize: 14, fontWeight: '500', color: 'red'}}>
+                                Cancel
+                            </Button>
+                        </View>
+                    </View>
+                </Modal>   
                 <Modal isVisible={this.state.modalVisible}
                     onBackButtonPress={() => {
                         this.setState({ modalVisible: false });
